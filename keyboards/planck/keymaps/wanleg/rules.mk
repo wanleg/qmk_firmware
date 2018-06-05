001738893fadc59aa57df6551f1b76c38ca2438d
wanleg <32079073+wanleg@users.noreply.github.com>
2018-06-04 18:25:04 -0700

    disable audio on Planck to save space

--- keyboards/planck/keymaps/wanleg/rules.mk
+++ keyboards/planck/keymaps/wanleg/rules.mk
@@ -4,3 +4,9 @@ endif
 
 TAP_DANCE_ENABLE = yes
 MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+
+###use to save space
+EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
+CONSOLE_ENABLE = no        # Console for debug(+400)
+NKRO_ENABLE = no          # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+AUDIO_ENABLE = no           # Audio output on port C6
