f5efa27590d29f500f49d443eedd271e44e9589a
admin <admin@mbp.local>
2018-06-30 01:02:23 -0700

    wanleg personal boards restore

---
+++ keyboards/planck/keymaps/wanleg/rules.mk
@@ -0,0 +1,10 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes	# Mouse keys(+4700)
+
+###use to save space
+CONSOLE_ENABLE = no        # Console for debug(+400)
+DEFAULT_FOLDER = planck/rev4
