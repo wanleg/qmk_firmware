301802e8600820af0f9ab074e2ebcd7f5ba137bb
wanleg <wanleg@github.com>
2019-05-10 09:55:17 -0700

    kbd6x keymap chg

--- keyboards/kbdfans/kbd6x/keymaps/wanleg/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/wanleg/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(ONE),
-	  KC_LCTL, MO(ONE), KC_LGUI,                   KC_SPACE,                   KC_RGUI, KC_RALT, KC_RCTL
+	  KC_LCTL, MO(ONE), KC_LGUI,                   LT(ONE,KC_SPACE),                   KC_RGUI, KC_RALT, KC_RCTL
       ),
 
 [ONE] = LAYOUT_wrapper(
@@ -77,6 +77,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, BL_BRTG, BL_DEC,  BL_INC,  BL_TOGG, BL_STEP,  BL_ON,   KC_PGUP, KC_HOME, _______, _______, _______, _______, _______,
       _______, RGB_M_B, RGB_VAD, RGB_VAI, RGB_TOG, RGB_MOD,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
       _______, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, _______,  _______, KC_PGDN, KC_END,  _______, gGHERKIN,_______, _______,
-	    _______, _______, _______,                   _______,                    _______, _______, _______
+	  _______, _______, _______,                   _______,                    _______, _______, _______
       ),
 };
