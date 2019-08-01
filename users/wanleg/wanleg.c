60b587fc5f31b9261daf3c408ef3029d1c8f15ad
wanleg <wanleg@github.com>
2019-08-01 14:10:10 -0700

    kdb6x backlighting tweak

--- users/wanleg/wanleg.c
+++ users/wanleg/wanleg.c
@@ -170,7 +170,9 @@ void matrix_init_user(void) {
 void keyboard_post_init_user(void) {
   #if defined(KEYBOARD_kbdfans_kbd6x)
     set_single_persistent_default_layer(_QW);
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 5);
-    rgblight_sethsv_noeeprom(0,0,128);
+    //rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 5);
+    //rgblight_sethsv_noeeprom(0,0,128);
+	rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING);
+	rgblight_sethsv_noeeprom(0,0,255);    
   #endif
 }
