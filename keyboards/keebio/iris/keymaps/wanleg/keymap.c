d1f248fe9afebf6081d09f3eda8440a80643d256
wanleg <wanleg@github.com>
2019-12-10 17:50:21 -0800

    add back onehand to iris keymap

--- keyboards/keebio/iris/keymaps/wanleg/keymap.c
+++ keyboards/keebio/iris/keymaps/wanleg/keymap.c
@@ -29,8 +29,8 @@
      K01,	K02,	K03,	K04,	K05,	K06,						K07,	K08,	K09,	K0A,	K0B,	K0C, \
      K11,	K12,	K13,	K14,	K15,	K16,						K17,	K18,	K19,	K1A,	K1B,	K1C, \
      K21,	K22,	K23,	K24,	K25,	K26,						K27,	K28,	K29,	K2A,	K2B,	K2C, \
-     K31,	K32,	K33,	K34,	K35,	K36,	_______, _______,	K37,	K38,	K39,	K3A,	K3B,	K3C, \
-                               _______, NUMBER, ETCETERA,			LT(_FN,KC_SPC), DIRECTION, KC_RGUI                  \
+     K31,	K32,	K33,	K34,	K35,	K36,	SH_MON, SH_MON,		K37,	K38,	K39,	K3A,	K3B,	K3C, \
+                               KC_SPC, NUMBER, ETCETERA,			LT(_FN,KC_SPC), DIRECTION, KC_RGUI                  \
   )
 #define LAYOUT_iris_base_wrapper(...)       LAYOUT_iris_base(__VA_ARGS__)
 
