5669088b97faf3f9cb27500c780d4f545496f3f5
wanleg <wanleg@github.com>
2021-02-02 09:47:18 -0800

    corne miniaxe copy

--- layouts/community/split_3x6_3/wanleg/keymap.c
+++ layouts/community/split_3x6_3/wanleg/keymap.c
@@ -1,22 +1,42 @@
 #include QMK_KEYBOARD_H
 #include "wanleg.h"
 
+
+#define LAYOUT_split_3x6_3_base( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B  \
+  ) \
+  LAYOUT_split_3x6_3_wrapper( \
+    K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
+    K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
+      _______, NUMBER, ETCETERA,	KC_SPC, DIRECTION, KC_RGUI  \
+  )
+#define LAYOUT_split_3x6_3_base_wrapper(...)       LAYOUT_split_3x6_3_base(__VA_ARGS__)
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_GK] = LAYOUT_split_3x6_3_wrapper(
-  _______________GherkinLike_0_______________,
-  _______________GherkinLike_1_______________,
-  _______________GherkinLike_2_______________,
-  ONEHAND, NUMBER, ETCETERA, LT(_FN,KC_SPC),DIRECTION, KC_RGUI
-),
-[ONE] = LAYOUT_split_3x6_3_wrapper(
-  _______________Qwerty_Row__0_______________,
-  _______________Qwerty_Row__1_______________,
-  _______________Qwerty_Row__2_______________,
-  GHERKIN, SUBTER, SH_T(KC_SPC), SH_T(KC_SPC), SUPRA, KC_RGUI
-),
-[DIR] = LAYOUT_split_3x6_3_wrapper(
-  _____________DIRECTIONS_Row__0_____________,
-  _____________DIRECTIONS_Row__1_____________,
-  _____________DIRECTIONS_Row__2_____________,
-  _______, _______, ONEHAND, _______, _______, _______
+  [_GK] = LAYOUT_split_3x6_3_base_wrapper(
+    _______________GherkinLike_0_______________,
+    _______________GherkinLike_1_______________,
+    _______________GherkinLike_2_______________
+  ),
+  [NUM] = LAYOUT_split_3x6_3_wrapper(
+    _______________NUMBERS_Row_0_______________,
+    _______________NUMBERS_Row_1_______________,
+    _______________NUMBERS_Row_2_______________,
+    _______, _______, _______,	KC_ENT, KC_RSFT, _______
+  ),
+  [DIR] = LAYOUT_split_3x6_3_base_wrapper(
+    _____________DIRECTIONS_Row__0_____________,
+    _____________DIRECTIONS_Row__1_____________,
+    _____________DIRECTIONS_Row__2_____________
+  ),
+  [ETC] = LAYOUT_split_3x6_3_wrapper(
+    ______________ETCETERA_Row__0______________,
+    ______________ETCETERA_Row__1______________,
+    ______________ETCETERA_Row__2______________,
+    _______, NUMBER, ETCETERA,	LALT(LCTL(KC_DEL)), DIRECTION, KC_RGUI
+  )
 };
