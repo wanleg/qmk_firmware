004e09b06de260526142c9d562bb75dc02ea86ec
wanleg <wanleg@github.com>
2022-11-20 15:33:16 -0800

    combos for gherkin

--- layouts/community/ortho_3x10/wanleg/keymap.c
+++ layouts/community/ortho_3x10/wanleg/keymap.c
@@ -1,6 +1,41 @@
 #include QMK_KEYBOARD_H
 #include "wanleg.h"
 
+//Combo section start
+//list combos
+enum combo_events {
+  THUMB_SPACE,
+  //EM_EMAIL,
+  COMBO_LENGTH //this is a required line for the COMBO_COUNT delete
+};
+uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define in config.h and use this instead!
+//create keycombo name mappings
+const uint16_t PROGMEM twoKeySpace[] = {LT(_FN,KC_N), LT(gDIR,KC_M), COMBO_END};
+//const uint16_t PROGMEM email_combo[] = {KC_G, KC_H, COMBO_END};
+
+combo_t key_combos[] = {
+    [THUMB_SPACE] = COMBO_ACTION(twoKeySpace),
+    //COMBO(twoKeySpace, KC_SPC), //can define simple actions here
+    [EM_EMAIL] = COMBO_ACTION(email_combo), //complex actions can be described below
+};
+/* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+  switch(combo_index) {
+    case EM_EMAIL:
+      if (pressed) {
+        SEND_STRING("john.doe@example.com");
+      }
+      break;
+    case THUMB_SPACE:
+      if (pressed) {
+        tap_code16(KC_SPC);
+      }
+      break;
+  }
+}
+//Combo section end
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [gGK] = LAYOUT_ortho_3x10_wrapper(
   _______________Gherkin_Row_0_______________,
--- layouts/community/ortho_3x10/wanleg/rules.mk
+++ layouts/community/ortho_3x10/wanleg/rules.mk
@@ -1,2 +1,3 @@
 BOOTLOADER = qmk-dfu
 TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
